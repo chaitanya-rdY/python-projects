eab31a1606adb93e4fa40bf68638b5ef67aa672b
chaitanya-rdY <cr5638963@gmail.com>
2024-07-09 20:50:57 +0530

    dsa

--- hello.py
+++ hello.py
@@ -1,19 +1,6 @@
-#armstrong number 
-def armstrong_number(x,y):
-    for i in range(x,y+1):
-        j=str(i)
-        count=len(j)
-        sum=0
-        a=i
-        # print(count)
-        while(a>0):
-            ld=a%10
-            a=a//10
-            sum=sum+pow(ld,count)
-        if(sum==int(j)):
-            print(i)
-x=int(input("start: "))
-y=int(input("stop: "))
-armstrong_number(x,y)            
-
+s=input("name:")
+m=max(s)
+size=ascii(m)-ascii('a')
+print(size)
+hash=[0 for i in range()]
     
--- practice.py
+++ practice.py
@@ -1,3 +1,13 @@
-def names(*kids):
-    print(kids[1]+kids[0],kids[2])
-names(1,2,"string")
+# list=[]
+# n=int(input("size of an list: "))
+# for i in range(n):
+#     number=int(input("please enter any number:"))
+#     list.append(number)
+# list2=[0 for i in range(max(list)+1)]
+# for i in range(n):
+#     list2[list[i]]+=1
+# num=int(input("please enter any number to find the frequency:"))
+# print(list2[num])
+x="abcDBF"
+y=x.title()
+print(y.find('b'))
